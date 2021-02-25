294b54923fc228a185577526fa4ff5b2b2004cee
lvstr <serankucing@gmail.com>
2021-02-25 20:29:38 +0700

    added mongo callback

--- index.js
+++ index.js
@@ -29,6 +29,29 @@ mongoose.connect(`${process.env.MONGO_URI}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+mongoose.connection.on("connected", function () {
+  console.log("Mongoose default connection open to " + process.env.MONGO_URI);
+});
+
+// If the connection throws an error
+mongoose.connection.on("error", function (err) {
+  console.log("Mongoose default connection error: " + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on("disconnected", function () {
+  console.log("Mongoose default connection disconnected");
+});
+
+// If the Node process ends, close the Mongoose connection
+process.on("SIGINT", function () {
+  mongoose.connection.close(function () {
+    console.log(
+      "Mongoose default connection disconnected through app termination"
+    );
+    process.exit(0);
+  });
+});
 
 const color = (text, color) => {
   return !color ? chalk.green(text) : chalk.keyword(color)(text);
